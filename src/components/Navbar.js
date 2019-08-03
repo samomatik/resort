60e9ed6bc26f6c57621476ae385a6d2558a1f359
Samitha Padukkage Don <sam@samomtik.com>
2019-08-03 14:45:02 +1000

    Navbar links fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,11 +18,11 @@ export default class Navbar extends Component {
                         <Link to="/">
                             <img src={logo} alt="Beach Resort" />
                         </Link>
-                        <button type="button" className="nav-btn  onClick={this.handleToggle} ">
+                        <button type="button" className="nav-btn"  onClick={this.handleToggle}>
                             <FaAlignRight className="nav-icon" />
                         </button>
                     </div>
-                    <ul className={this.state.isOpen?"nav-links show-nav":"nav-links"}>
+                    <ul className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}>
                         <li>
                             <Link to="/">Home</Link>
                         </li>
