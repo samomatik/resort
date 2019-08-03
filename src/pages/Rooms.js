86e1b1246253cc6c67e23c013264c040fadd3f8d
Samitha Padukkage Don <sam@samomtik.com>
2019-08-03 14:53:05 +1000

    Hero images added

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Hero from '../components/Hero';
 
 const Rooms = () => {
     return (
         <div>
-            hello from rooms page
+            <Hero hero="roomsHero" />
         </div>
     )
 }
