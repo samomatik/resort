775c77f35b37794323a32e86018dc6a1ba31ba64
Samitha Padukkage Don <sam@samomtik.com>
2019-08-03 19:09:27 +1000

    Set up context and add data

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {RoomContext} from '../Context'
+import Loading from './Loading'
+
+export default class FeaturedRooms extends Component {
+    static contextType = RoomContext;
+    render() {
+        const {featuredRooms: rooms} = this.context;
+        console.log(rooms);
+        return (
+            <div>
+                hello from featured rooms
+                <Loading />
+            </div>
+        )
+    }
+}
