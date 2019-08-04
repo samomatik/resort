4955d9216f4e1086df176e68db0ca34f0d6e7e71
Samitha Padukkage Don <sam@samomtik.com>
2019-08-04 17:34:54 +1000

    Inividual room pages added

--- src/Context.js
+++ src/Context.js
@@ -17,9 +17,9 @@ class RoomProvider extends Component {
         let rooms = this.formatData(items);
         let featuredRooms = rooms.filter(room => room.featured === true);
         this.setState({
-            rooms, 
-            featuredRooms, 
-            sortedRooms:rooms, 
+            rooms,
+            featuredRooms,
+            sortedRooms: rooms,
             loading: false
         })
     }
@@ -28,16 +28,22 @@ class RoomProvider extends Component {
         let tempItems = items.map(item => {
             let id = item.sys.id;
             let images = item.fields.images.map(image => image.fields.file.url);
-            let room = {...item.fields, images,id}
+            let room = { ...item.fields, images, id }
 
             return room;
         });
         return tempItems;
     }
 
+    getRoom = (slug) => {
+        let tempRooms = [...this.state.rooms];
+        const room = tempRooms.find(room => room.slug === slug);
+        return room;
+    }
+
     render() {
         return (
-            <RoomContext.Provider value={{ ...this.state }}>
+            <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
                 {this.props.children}
             </RoomContext.Provider>
         )
