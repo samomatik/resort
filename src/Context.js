154779b0a66f543524925a3a970bca377b34ec7c
Samitha Padukkage Don <sam@samomtik.com>
2019-08-18 17:06:59 +1000

    Get data from Contentful and deploy to Netlify

--- src/Context.js
+++ src/Context.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import items from './data';
+// import items from './data'
+import Client from './Contentful'
 
 const RoomContext = React.createContext();
 
@@ -20,22 +21,33 @@ class RoomProvider extends Component {
         pets: false
     }
 
-    // getData
+    // Get data from Contentful
+    getData = async () => {
+        try {
+            let response = await Client.getEntries({
+                content_type: 'beachResortRooms',
+                order: 'fields.price'
+            });
+            let rooms = this.formatData(response.items);
+            let featuredRooms = rooms.filter(room => room.featured === true);
+            let maxPrice = Math.max(...rooms.map(item => item.price));
+            let maxSize = Math.max(...rooms.map(item => item.size));
+            this.setState({
+                rooms,
+                featuredRooms,
+                sortedRooms: rooms,
+                loading: false,
+                price: maxPrice,
+                maxPrice,
+                maxSize
+            })
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     componentDidMount() {
-        let rooms = this.formatData(items);
-        let featuredRooms = rooms.filter(room => room.featured === true);
-        let maxPrice = Math.max(...rooms.map(item => item.price));
-        let maxSize = Math.max(...rooms.map(item => item.size));
-        this.setState({
-            rooms,
-            featuredRooms,
-            sortedRooms: rooms,
-            loading: false,
-            price: maxPrice,
-            maxPrice,
-            maxSize
-        })
+        this.getData();
     }
 
     formatData(items) {
